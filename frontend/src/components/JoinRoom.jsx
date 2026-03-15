fea61589c805a52b9871535a256b41c6254263b6
Arghya Chakraborty <arghya0812@gmail.com>
2026-03-15 22:53:35 +0530

    join page structure

---
+++ frontend/src/components/JoinRoom.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function JoinRoom() {
+  return (
+    <div>
+      {/* Full-screen parent container */}
+        <div className='flex justify-center items-center h-screen w-full bg-slate-800'>
+    
+            {/* Your centered box */}
+            <div className="bg-zinc-700 h-3/6 w-6/12 flex justify-center items-center text-white border-2 border-">
+                inside box
+            </div>
+
+        </div>
+    </div>
+  )
+}
